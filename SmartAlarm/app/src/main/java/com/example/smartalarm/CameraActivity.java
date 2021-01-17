f3ffcbf8a7486c9f5c3490a3dc21187d49536f78
ihsan <iyazici@ku.edu.tr>
2021-01-17 21:44:05 +0300

    added camera activity

--- SmartAlarm/app/src/main/java/com/example/smartalarm/CameraActivity.java
+++ SmartAlarm/app/src/main/java/com/example/smartalarm/CameraActivity.java
@@ -3,12 +3,28 @@ package com.example.smartalarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.camerakit.CameraKitView;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private CameraKitView cameraView;
+    private Button faceDetectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        faceDetectButton = findViewById(R.id.detect_gesture_btn);
+        cameraView = findViewById(R.id.camera_view);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        cameraView.stop();
     }
 }
--- SmartAlarm/app/src/main/java/com/example/smartalarm/RingActivity.java
+++ SmartAlarm/app/src/main/java/com/example/smartalarm/RingActivity.java
@@ -2,7 +2,10 @@ package com.example.smartalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -11,9 +14,12 @@ import java.util.Calendar;
 public class RingActivity extends AppCompatActivity {
 
     public TextView ring_current_time,ring_current_date;
+    public Button open_camera_button;
     private Calendar calendar;
     private SimpleDateFormat dateFormat;
 
+    //  Booleans
+    public boolean switched_to_cam_act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,7 @@ public class RingActivity extends AppCompatActivity {
 
         ring_current_time = findViewById(R.id.current_time_text);
         ring_current_date = findViewById(R.id.current_date_text);
+        open_camera_button = findViewById(R.id.gesture_button);
 
         ReturnCurrentDate();
         ReturnCurrentTime();
@@ -40,5 +47,22 @@ public class RingActivity extends AppCompatActivity {
         dateFormat = new SimpleDateFormat("EEE MMM d");
         String date = dateFormat.format(calendar.getTime());
         ring_current_date.setText(date);
+    }
+
+    //  Switched to Camera Activity
+    public void SwitchToCameraActivity()
+    {
+        if(!switched_to_cam_act)
+        {
+            Intent intent = new Intent(RingActivity.this,CameraActivity.class);
+            startActivity(intent);
+            switched_to_cam_act = true;
+        }
+    }
+
+    //  OnClickListener for camera button
+    public void CameraButton(View view)
+    {
+        SwitchToCameraActivity();
     }
 }
