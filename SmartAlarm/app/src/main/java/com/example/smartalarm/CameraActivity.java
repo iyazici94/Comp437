7a9fbe0632994820d3d428d4088717d8945c57fd
ihsan <iyazici@ku.edu.tr>
2021-01-18 01:39:00 +0300

    camera properly works, need to work on input image for smile detection

--- SmartAlarm/app/src/main/java/com/example/smartalarm/CameraActivity.java
+++ SmartAlarm/app/src/main/java/com/example/smartalarm/CameraActivity.java
@@ -1,11 +1,31 @@
 package com.example.smartalarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.PointF;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.camerakit.CameraKitView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.face.FaceLandmark;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -19,12 +39,155 @@ public class CameraActivity extends AppCompatActivity {
 
         faceDetectButton = findViewById(R.id.detect_gesture_btn);
         cameraView = findViewById(R.id.camera_view);
+
+        FirebaseApp.initializeApp(this);
+
+    }
+
+
+    public void MyCameraListener(View view)
+    {
+        View.OnClickListener photoOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.captureImage(new CameraKitView.ImageCallback() {
+                    @Override
+                    public void onImage(CameraKitView cameraKitView, byte[] capturedImage) {
+                        File savedPhoto = new File(Environment.getExternalStorageDirectory(),"photo.jpg");
+                        try {
+                            FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                            outputStream.write(capturedImage);
+                            outputStream.close();
+                            InputImage image = imageFromArray(capturedImage,0);
+                            ProcessFaceDetection(image);
+                        }
+                        catch (java.io.IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+
+                });
+            }
+        };
+    }
+
+    public void ProcessFaceDetection(InputImage image)
+    {
+        // [START set_detector_options]
+        FaceDetectorOptions options =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                        .setMinFaceSize(0.15f)
+                        .enableTracking()
+                        .build();
+        // [END set_detector_options]
+
+        // [START get_detector]
+        FaceDetector detector = FaceDetection.getClient(options);
+
+        // [START run_detector]
+        Task<List<Face>> result =
+                detector.process(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<Face>>() {
+                                    @Override
+                                    public void onSuccess(List<Face> faces) {
+                                        // Task completed successfully
+                                        // [START_EXCLUDE]
+                                        // [START get_face_info]
+                                        for (Face face : faces) {
+                                            Rect bounds = face.getBoundingBox();
+                                            float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
+                                            float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
+
+
+                                            if(faces.isEmpty())
+                                            {
+                                                int duration = Toast.LENGTH_SHORT;
+
+                                                Toast toast = Toast.makeText(getApplicationContext(),"No face detected",duration);
+                                                toast.show();
+                                            }
+                                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                                            // nose available):
+                                            FaceLandmark leftEar = face.getLandmark(FaceLandmark.LEFT_EAR);
+                                            if (leftEar != null) {
+                                                PointF leftEarPos = leftEar.getPosition();
+                                            }
+
+                                            // If classification was enabled:
+                                            if (face.getSmilingProbability() != null) {
+                                                float smileProb = face.getSmilingProbability();
+                                                //  THIS CODE MIGHT CAUSES ERRORS, TEST THIS!!!!
+                                                if(smileProb >= 0.5)
+                                                    SwitchToMainActivity();
+                                            }
+                                            if (face.getRightEyeOpenProbability() != null) {
+                                                float rightEyeOpenProb = face.getRightEyeOpenProbability();
+                                            }
+
+                                            // If face tracking was enabled:
+                                            if (face.getTrackingId() != null) {
+                                                int id = face.getTrackingId();
+                                            }
+                                        }
+                                        // [END get_face_info]
+                                        // [END_EXCLUDE]
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+    }
+
+
+    public void SwitchToMainActivity()
+    {
+        Intent intent = new Intent(CameraActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+    private InputImage imageFromArray(byte[] byteArray, int rotation) {
+        // [START image_from_array]
+        InputImage image = InputImage.fromByteArray(
+                byteArray,
+                /* image width */480,
+                /* image height */360,
+                rotation,
+                InputImage.IMAGE_FORMAT_NV21 // or IMAGE_FORMAT_YV12
+        );
+        // [END image_from_array]
+        return image;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraView.onStart();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        cameraView.onPause();
+    }
 
-        cameraView.stop();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cameraView.onStop();
     }
 }
